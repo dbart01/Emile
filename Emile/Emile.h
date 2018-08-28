df40c37f1d91675335ed32d798c3372e2736e655
Dima Bart <dima.bart01@gmail.com>
2018-08-28 15:49:13 -0400

    Initial Commit

---
+++ Emile/Emile.h
@@ -0,0 +1,19 @@
+//
+//  Emile.h
+//  Emile
+//
+//  Created by Dima Bart on 2018-08-28.
+//  Copyright © 2018 Dima Bart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Emile.
+FOUNDATION_EXPORT double EmileVersionNumber;
+
+//! Project version string for Emile.
+FOUNDATION_EXPORT const unsigned char EmileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Emile/PublicHeader.h>
+
+
